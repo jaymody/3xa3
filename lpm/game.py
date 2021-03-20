e79a11d2021e58ff6abca7a72c0978f8e0b6ea5a
Jessica Anne Lim <limj31@mcmaster.ca>
2021-03-20 19:22:40 -0400

    initial code for game.py
    Co-authored-by: Maanav Dalal <dalalm1@mcmaster.ca>

--- lpm/game.py
+++ lpm/game.py
@@ -1,5 +1,9 @@
 """Module that contains main logic for lpm typing game."""
 
+from .screen import Screen
+from .stats import Stat
+from .config import Config
+
 
 class Game:
     def __init__(self, snippets, screen, stats):
@@ -18,6 +22,7 @@ class Game:
         self.screen = screen
         self.stats = stats
         self.current_stat = None
+        self.current_position = [0, 0]
         self.state = 0
 
     def run(self):
@@ -28,25 +33,25 @@ class Game:
             self.state = self.get_state(key)
 
             # # only do rendering stuff here
-            # if state == 0:
-            #     # user is browsing
-            #     # must exit using ctrl + c
-            #     self.browsing()
-            # if state == 1:
-            #     # reading user input
-            #     self.typing()
-            # elif state == 2:
-            #     # show stats for this snippet
-            #     self.done()
-            # elif state == 3:
-            #     # resize screen
-            #     self.screen.resize()
-            # elif state == -1:
-            #     # throw keyboard error which exits
-            #     pass
-            # else:
-            #     # panic??????!!!
-            #     pass
+            if self.state == 0:
+                # resize screen
+                self.screen.resize()
+            if self.state == 1:
+                # user is browsing
+                # must exit using ctrl + c
+                self.browsing(key)
+            if self.state == 2:
+                # reading user input
+                self.typing(key)
+            elif self.state == 3:
+                # show stats for this snippet
+                self.done(key)
+            elif self.state == -1:
+                # throw keyboard error which exits
+                self.save_game()
+                raise KeyboardInterrupt
+            else:
+                raise Exception("wtf")
 
     def get_state(self, key):
         """Get the state of the game.
@@ -68,16 +73,88 @@ class Game:
         int
             Current state of the game.
         """
-        pass
+        keys = {Screen.KEY_BACKSPACE, Screen.KEY_ESCAPE}
 
-    def typing(self):
+        if key == Screen.KEY_RESIZE:
+            return 0
+        elif self.current_stat is None:
+            if key == Screen.KEY_LEFT or key == Screen.KEY_RIGHT:  # TODO spacebar
+                return 1
+            elif key not in keys:
+                # start a new game
+                self.start_snippet()
+                return 2
+        elif (
+            self.current_stat.start_time() is not None
+            and self.current_stat.end_time() is None
+        ):
+            return 2
+        elif (
+            self.current_stat.start_time() is not None
+            and self.current_stat.end_time() is not None
+        ):
+            return 3
+        else:
+            return -1  ## Eventually be exit
+
+    def typing(self, key):
         """Handles interaction during the typing (gameplay) state."""
-        pass
+        current_entry = self.snippets.current_entry()
+        current_snippet = self.snippets.getItem(current_entry)
+        current_line = current_snippet.lines[self.current_position[0]]
+        current_char = current_line[self.current_position[1]]
+        if key == None:
+            pass
+        elif key == Screen.KEY_ENTER:  # Go to next line
+            self.current_position[0] += 1
+        elif key == Screen.KEY_BACKSPACE:  # Go back one character
+            self.stats.num_wrong -= 1
+            self.current_position[1] -= 1
+        elif key == Screen.KEY_ESCAPE:  # Stop typing
+            # Figure this out later
+            # TODO:
+            pass
+        else:  # key is a typed key
+            # If we made it to the end, abort
+            if current_line == len(current_snippet.lines) and current_char == len(
+                current_line
+            ):  # this might be +1
+                self.finished_snippet()
 
-    def done(self):
+            if key == current_char:
+                # Right
+                self.stats.num_right += 1
+
+            else:
+                # Wrong
+                self.stats.num_wrong += 1
+
+            self.current_position[1] += 1
+
+    def start_snippet(self):
+        self.current_stat = Stat()
+        self.current_stat.start()
+
+    def finished_snippet(self):
+        self.current_stat.stop()
+        self.stats.update(self.current_stat)
+
+    def done(self, key):
         """Handles interaction during done state."""
-        pass
+        if key == Screen.KEY_LEFT or key == Screen.KEY_RIGHT:  # TODO spacebar
+            self.browsing(key)
 
-    def browsing(self):
+    def browsing(self, key):
         """Handles interactio during the browsing state."""
-        pass
+        if key == Screen.KEY_LEFT:
+            self.snippets.prev_entry()
+        elif key == Screen.KEY_RIGHT:
+            self.snippets.next_entry()
+        self.reset()
+
+    def save_game(self):
+        self.stats.save(Config.STATS_PATH)
+
+    def reset(self):
+        """Resets stats and browses"""
+        self.current_stat = None
