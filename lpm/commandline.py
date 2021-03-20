047b6e6dac550d32d06c7946a9a514e3c6451ca3
Maanav Dalal <dalalm1@mcmaster.ca>
2021-03-20 16:52:59 -0400

    Resolve "Implement `commandline.py`"

--- lpm/commandline.py
+++ lpm/commandline.py
@@ -3,48 +3,83 @@
 Use `lpm -h` for help.
 """
 
+import os
 import argparse
+
+from . import __version__
+from .config import Config
+from .snippets import Snippets
+from .screen import Screen
+from .stats import Stats
+from .game import Game
 
 
 def stats():
     """Displays the users statistics to the command-line."""
-    print("pi = 3.14")
-    # display lifetime stats
-    pass
+    print("==================================================================")
+    print("Games | Avg LPM | Avg Accuracy | Avg CPM | Lifetime Elapsed Time")
+    # print(f"{} | {} | {} | {} | {}")
+    print("==================================================================")
 
 
 def start():
     """Starts the lpm typing interface."""
-    # load stats
-    # load snippets
-    # load screen
-    # create game
+    snippets = Snippets.load(Config.SNIPPETS_PATH)
+    screen = Screen()
+    statistics = Stats.load(Config.STATS_PATH)
+    game = Game(snippets, screen, statistics)
     try:
-        # game.run()
-        pass
+        game.run()
+
+        # technically, this line should never be reached
+        statistics.save(Config.STATS_PATH)
     except KeyboardInterrupt:
         # save stats, clear screen or something, quit game
-        pass
+        statistics.save(Config.STATS_PATH)
 
 
 def settings():
     """Allows user to modify lpm settings."""
-    # open vim with CONFIG_PATH
-    pass
+    editor = os.environ.get("EDITOR", "vim")
+    os.system(f"{editor} {Config.CONFIG_PATH}")
 
 
 def reset():
     """Resets the settings for lpm."""
-    # ask if they want to reset config (y/n), call Config.reset
-    # ask if they want to delete stats (y/n) delete the stats file
-    pass
+    config_reset = input("Do you want to reset your config? (y/n) ")
+    if config_reset == "y" or config_reset == "Y":
+        Config.reset()
+        print("User settings were reset using defaults. \n")
+
+    stats_reset = input("Do you want to reset your lifetime statistics? (y/n) ")
+    if stats_reset == "y" or stats_reset == "Y":
+        if os.path.exists(Config.STATS_PATH):
+            os.remove(Config.STATS_PATH)
+            print("User statistics were reset. \n")
+        else:
+            print("User statistics do not exist. \n")
 
 
 def cli():
     """Main entry point for lpm CLI."""
     parser = argparse.ArgumentParser("Lines Per Minute")
-    parser.add_argument("--stats", action="store_true")
+    parser.add_argument("-s", "--stats", action="store_true", help=stats.__doc__)
+    parser.add_argument(
+        "-v", "--version", action="store_true", help="Show program version"
+    )
+    parser.add_argument("--reset", action="store_true", help=reset.__doc__)
+    parser.add_argument("--settings", action="store_true", help=settings.__doc__)
+
     args = parser.parse_args()
 
     if args.stats:
         stats()
+    elif args.version:
+        print(f"Current version: {__version__}")
+    elif args.reset:
+        reset()
+    elif args.settings:
+        settings()
+    else:
+        # panik
+        pass
