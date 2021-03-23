c9621ab0549cca974a4f667fafdaaade65060152
jaymody <jaykmody@gmail.com>
2021-03-22 22:32:31 -0400

    bump to version 0.0.4

--- lpm/__init__.py
+++ lpm/__init__.py
@@ -1,6 +1,6 @@
 """Lines Per Minute."""
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 # TODO: if a snippet uses tabs, convert to spaces
 # "https://github.com/jaymody/linkipedia/blob/09f3ca27e1ad858a6a010d2ef3d0768cbb9dda36/src/main/java/com/linkipedia/Graph.java#L9-L31",
