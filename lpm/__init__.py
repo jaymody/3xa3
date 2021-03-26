16e98330f11b0c9ec109eba9bd4708f39411e9f6
Maanav Dalal <dalalm1@mcmaster.ca>
2021-03-25 20:45:08 -0400

    added additional code snippets

--- lpm/__init__.py
+++ lpm/__init__.py
@@ -8,4 +8,18 @@ _github_permalink = [
     "https://github.com/jaymody/seq2seq_polynomial/blob/b3fc25121a1210b98a2fa6efec33af539812cad4/tests.py#L4-L14",
     "https://github.com/jaymody/Brawler64/blob/632b98b61e9ceeb16742926b57cc4a98a364abd3/src/game/KeyInput.java#L1-L18",
     "https://github.com/jaymody/jaymody.github.io/blob/84598e192475bb2be161994cd1543a83b0737acf/src/components/Deck.js#L1-L18",
+    "https://github.com/MaanavD/Employmint/blob/f77551a2225c67891cc48c1d8014865d3d05d4b4/app/src/main/java/thacks/employmint/jobsearch.java#L79-L87",
+    "https://github.com/MaanavD/TLDR-Bot/blob/cd026ba24cb3f329bbedb135a9a8c168409b7cea/stdlib/MaanavD/slack-app/functions/commands/tldr.js#L24-L33",
+    "https://github.com/MaanavD/TLDR-Bot/blob/cd026ba24cb3f329bbedb135a9a8c168409b7cea/stdlib/MaanavD/slack-app/functions/commands/tldr.js#L77-L87",
+    "https://github.com/cslarsen/wpm/blob/6e48d8b750c7828166b67a532ff03d62584fb953/wpm/histogram.py#L44-L58",
+    "https://github.com/MaanavD/TLDR-Bot/blob/cd026ba24cb3f329bbedb135a9a8c168409b7cea/stdlib/MaanavD/slack-app/utils/format_message.js#L37-L48"
+    "https://github.com/MaanavD/SN8KRS/blob/a77b45e7539e5ee00875b2e7f20969a46410e958/models/shoe.model.js#L5-L15",
+    "https://github.com/MaanavD/SN8KRS/blob/a77b45e7539e5ee00875b2e7f20969a46410e958/routes/offers.js#L4-L11",
+    "https://github.com/MaanavD/SN8KRS/blob/a77b45e7539e5ee00875b2e7f20969a46410e958/client/src/App.js#L14-L23",
+    "https://github.com/MaanavD/SN8KRS/blob/a77b45e7539e5ee00875b2e7f20969a46410e958/client/src/index.js#L9-L14",
+    "https://github.com/JessicaLim8/Linkipedia/blob/8880a1b8e6b08add67cdc4ad827c98d9bf7871f5/src/main/java/com/linkipedia/Node.java#L11-L23",
+    "https://github.com/JessicaLim8/VoicePrep/blob/975092f57365009bc3d4d2c2b78e1a2bd0b2bcca/client/src/pages/Results.js#L26-L34",
+    "https://github.com/JessicaLim8/VoicePrep/blob/975092f57365009bc3d4d2c2b78e1a2bd0b2bcca/client/src/pages/wordcounter.js#L1-L14",
+    "https://github.com/JessicaLim8/BrAsthma/blob/0d00169ee73be77a056dae1dd00e8699d31cb028/dp3.py#L32-L41",
+    "https://github.com/JessicaLim8/BrAsthma/blob/0d00169ee73be77a056dae1dd00e8699d31cb028/practice_mock.py#L1-L11",
 ]
